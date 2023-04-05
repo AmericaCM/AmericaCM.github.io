debc8e47d289a77ce45ceafc7af7a29d904b20d1
America Cruz <128936432+AmericaCM@users.noreply.github.com>
2023-04-04 19:28:15 -0600

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let typewriter = new Typewriter(app, {
  
 typewriter
   .pauseFor(2000)
-  .typeString('<span style="color: #c5448f;"> Estudiante de Ingenieria en Mecatronica en la UPA.</span>')
+  .typeString('<span style="color: #a744c5;"> Estudiante de Ingenieria en Mecatronica en la UPA.</span>')
   .pauseFor(500)
   .deleteChars(10)
   .start();
